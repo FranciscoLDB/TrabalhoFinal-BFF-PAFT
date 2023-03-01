1223fcc484f6bcd2e91afef012f41852863e6406
franciscoldb <fluis905@gmail.com>
2023-02-28 21:10:09 -0300

    add arquivos

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
 import requests
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/pokemon')
 def pokemon():
