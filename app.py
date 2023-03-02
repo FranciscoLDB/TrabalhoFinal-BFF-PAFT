a0dba42ef1e841813840b6507a29a1ebca02c121
franciscoldb <fluis905@gmail.com>
2023-03-02 10:48:20 -0300

    add discover jinja

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ def github():
 @app.route('/searchfilme')
 def searchfilme():
     filme = request.args.get('filme')
+    if filme is None:
+        return {'message': 'nenhum filme encontrado'}
+    
     urlSearch = 'https://api.themoviedb.org/3/search/movie?api_key=a52d564a84702ae0175b97bb00f173af&query=' + filme
     respSearch = requests.get(urlSearch)
     jsonSearch = respSearch.json()
@@ -56,5 +59,21 @@ def searchfilme():
         ]
     }
     return resp
+
+@app.route('/jinja')
+def jinja():
+    respFilme = requests.get('https://api.themoviedb.org/3/discover/movie?api_key=a52d564a84702ae0175b97bb00f173af&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
+    jsonFilme = respFilme.json()
+
+    filmes = []
+    if jsonFilme.get('results') is None:
+        return render_template('gitJinja.html', erro = 'Falha ao buscar na api')
+    for i in range(18):
+        filmes.append({
+            'titulo': jsonFilme.get('results')[i].get('original_title'),
+            'foto': 'https://image.tmdb.org/t/p/w500' + jsonFilme.get('results')[i].get('poster_path'),
+        })
+
+    return render_template('gitJinja.html', filmes = filmes)
 
 app.run(debug=True)
