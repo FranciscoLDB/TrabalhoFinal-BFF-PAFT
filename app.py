980b0413d9de65909b611e3f9b41738e4f92558a
franciscoldb <fluis905@gmail.com>
2023-03-03 11:36:00 -0300

    ajuste erros git e filmes

--- app.py
+++ app.py
@@ -10,55 +10,61 @@ def index():
 
 @app.route('/github')
 def github():
-    usuario = request.args.get('usuario')
-    respGit = requests.get('https://api.github.com/users/' + usuario)
-    jsonGit = respGit.json()
-
-    resp = {
-        'nome': jsonGit.get('name'),
-        'usuario': jsonGit.get('login'),
-        'bio': jsonGit.get('bio'),
-        'local': jsonGit.get('location'),
-        'foto': jsonGit.get('avatar_url'),
-    }
-    return resp
+    if 'usuario' in request.args and request.args.get('usuario') != '':
+        usuario = request.args.get('usuario')
+        respGit = requests.get('https://api.github.com/users/' + usuario)
+        jsonGit = respGit.json()
+        if 'message' in jsonGit:
+            return jsonify({'message':'Usuario não localizado'}), 404
+        resp = {
+            'nome': jsonGit.get('name'),
+            'usuario': jsonGit.get('login'),
+            'bio': jsonGit.get('bio'),
+            'local': jsonGit.get('location'),
+            'foto': jsonGit.get('avatar_url'),
+        }
+        return jsonify(resp), 200
+    
+    return jsonify({'message':'Bad request'}), 400
 
 @app.route('/searchfilme')
 def searchfilme():
-    filme = request.args.get('filme')
-    if filme is None:
-        return {'message': 'nenhum filme encontrado'}
-    
-    urlSearch = 'https://api.themoviedb.org/3/search/movie?api_key=a52d564a84702ae0175b97bb00f173af&query=' + filme
-    respSearch = requests.get(urlSearch)
-    jsonSearch = respSearch.json()
-    
-    urlSimilar = 'https://api.themoviedb.org/3/movie/' + str(jsonSearch.get('results')[0].get('id')) + '/similar?api_key=a52d564a84702ae0175b97bb00f173af&language=en-US&page=1'
-    respSimilar = requests.get(urlSimilar)
-    jsonSimilar = respSimilar.json()
+    if 'filme' in request.args and request.args.get('filme') != '':
+        filme = request.args.get('filme')
 
-    resp = {
-        'titulo': jsonSearch.get('results')[0].get('original_title'),
-        'data': jsonSearch.get('results')[0].get('release_date'),
-        'foto': 'https://image.tmdb.org/t/p/w500' + jsonSearch.get('results')[0].get('poster_path'),
-        'nota': jsonSearch.get('results')[0].get('vote_average'),
-        'descricao': jsonSearch.get('results')[0].get('overview'),
-        'similar': [
-            {
-                'titulo': jsonSimilar.get('results')[0].get('original_title'),
-                'foto': 'https://image.tmdb.org/t/p/w500' + jsonSimilar.get('results')[0].get('poster_path'),
-            },
-            {
-                'titulo': jsonSimilar.get('results')[1].get('original_title'),
-                'foto': 'https://image.tmdb.org/t/p/w500' + jsonSimilar.get('results')[1].get('poster_path'),
-            },
-            {
-                'titulo': jsonSimilar.get('results')[2].get('original_title'),
-                'foto': 'https://image.tmdb.org/t/p/w500' + jsonSimilar.get('results')[2].get('poster_path'),
-            },
-        ]
-    }
-    return resp
+        urlSearch = 'https://api.themoviedb.org/3/search/movie?api_key=a52d564a84702ae0175b97bb00f173af&query=' + filme
+        respSearch = requests.get(urlSearch)
+        jsonSearch = respSearch.json()
+        if jsonSearch.get('total_results') == 0:
+            return jsonify({'message':'Nenhum filme localizado'}), 404      
+        
+        urlSimilar = 'https://api.themoviedb.org/3/movie/' + str(jsonSearch.get('results')[0].get('id')) + '/similar?api_key=a52d564a84702ae0175b97bb00f173af&language=en-US&page=1'
+        respSimilar = requests.get(urlSimilar)
+        jsonSimilar = respSimilar.json()
+
+        resp = {
+            'titulo': jsonSearch.get('results')[0].get('original_title'),
+            'data': jsonSearch.get('results')[0].get('release_date'),
+            'foto': 'https://image.tmdb.org/t/p/w500' + jsonSearch.get('results')[0].get('poster_path'),
+            'nota': jsonSearch.get('results')[0].get('vote_average'),
+            'descricao': jsonSearch.get('results')[0].get('overview'),
+            'similar': [
+                {
+                    'titulo': jsonSimilar.get('results')[0].get('original_title'),
+                    'foto': 'https://image.tmdb.org/t/p/w500' + jsonSimilar.get('results')[0].get('poster_path'),
+                },
+                {
+                    'titulo': jsonSimilar.get('results')[1].get('original_title'),
+                    'foto': 'https://image.tmdb.org/t/p/w500' + jsonSimilar.get('results')[1].get('poster_path'),
+                },
+                {
+                    'titulo': jsonSimilar.get('results')[2].get('original_title'),
+                    'foto': 'https://image.tmdb.org/t/p/w500' + jsonSimilar.get('results')[2].get('poster_path'),
+                },
+            ]
+        }
+        return jsonify(resp), 200
+    return jsonify({'message':'Bad request'}), 400
 
 @app.route('/jinja')
 def jinja():
--- static/script.js
+++ static/script.js
@@ -1,18 +1,24 @@
-console.log('script works!');
-
 function buscarGit(){
     console.log('Buscando git!');
     let usuario = document.getElementById('input_git').value;
+    
     fetch('http://127.0.0.1:5000/github?usuario=' + usuario)
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            if(data.message){
+                window.alert('Houve um erro ao buscar usuario: ' + data.message);
+                return
+            }
             document.getElementById('foto_git').setAttribute('src', data.foto);
             document.getElementById('nome_git').innerText = data.nome;
             document.getElementById('usuario_git').innerText = data.usuario;
             document.getElementById('bio_git').innerText = data.bio;
             document.getElementById('location_git').innerText = data.local;
-        })
+        }).catch(error => {
+            console.log('Erro: ' + error);
+            window.alert('Houve um erro ao mostrar usuario');
+        });
 }
 
 function buscarFilme(){
@@ -22,6 +28,10 @@ function buscarFilme(){
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            if(data.message){
+                window.alert('Erro ao buscar filme: ' + data.message);
+                return
+            }
             document.getElementById('foto_filme').setAttribute('src', data.foto);
             document.getElementById('nome_filme').innerText = data.titulo;
             document.getElementById('data_filme').innerText = data.data;
@@ -33,5 +43,8 @@ function buscarFilme(){
             document.getElementById('nome_similar_2').innerText = data.similar[1].titulo;
             document.getElementById('foto_similar_3').setAttribute('src', data.similar[2].foto);
             document.getElementById('nome_similar_3').innerText = data.similar[2].titulo;
-        })
+        }).catch(error => {
+            console.log('Erro: ' + error);
+            window.alert('Houve um erro ao mostrar filme');
+        });
 }
